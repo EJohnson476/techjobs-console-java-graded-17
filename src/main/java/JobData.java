3e9732915da10765b269bd33edcae0d5911189b1
E J <johnsonelijah440@gmail.com>
2023-12-13 22:08:06 -0600

    <GA1>

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -75,7 +75,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -94,8 +94,22 @@ public class JobData {
         // load data, if not already loaded
         loadData();
 
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for(HashMap<String, String> row : allJobs)
+        {
+            for(String key : row.keySet())
+            {
+                String value1 = row.get(key);
+                if(value1.toLowerCase().contains(value.toLowerCase()) || key.toLowerCase().contains(value.toLowerCase()))
+                {
+                    jobs.add(row);
+                }
+            }
+        }
+
         // TODO - implement this method
-        return null;
+        return jobs;
     }
 
     /**
--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,22 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if(someJobs.isEmpty())
+        {
+            System.out.println("No Results");
+            return;
+        }
 
-        System.out.println("printJobs is not implemented yet");
+        for(HashMap<String,String> job : someJobs)
+        {
+            System.out.println("*****");
+            for(String key : job.keySet())
+            {
+                String value = job.get(key);
+                System.out.println(key + ": " + value);
+
+            }
+            System.out.println("*****\n");
+        }
     }
 }
